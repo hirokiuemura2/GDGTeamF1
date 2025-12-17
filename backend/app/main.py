79be3a9b78333aba2c2180fff9d4ac50eb50186c
Hiroki Uemura <hirokiuemura2@gmail.com>
2025-12-17 19:22:48 +0900

    Base API for data storage

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,18 @@
+import json
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
-
+class basicItem(BaseModel):
+    username: str
+    pwd: str
+    dataPack: json
 
 @app.get("/healthcheck")
 def healthcheck():
     return {"status": "ok"}
+
+@app.post("/postData")
+def push_notification():
+    
+    return {"message": "Push notification sent"}
