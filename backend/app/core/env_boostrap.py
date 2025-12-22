c52536d3a725b886d2b31f4a0fc72023f594c452
Heaven Chen <chenheaven@icloud.com>
2025-12-22 16:00:49 +0900

    chore(.env): Use secret manager to load config during runtime

---
+++ backend/app/core/env_boostrap.py
@@ -0,0 +1,16 @@
+import os
+from google.cloud import secretmanager
+
+
+def load_env_from_secret_manager(
+    *,
+    secret_name: str,
+    project_id: str,
+    target_path: str = ".env",
+) -> None:
+    client = secretmanager.SecretManagerServiceClient()
+    name = f"projects/{project_id}/secrets/{secret_name}/versions/latest"
+    response = client.access_secret_version(request={"name": name})
+    env_content = response.payload.data.decode("utf-8")
+    with open(target_path, "w") as f:
+        f.write(env_content)
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 
+from app.core.env_boostrap import load_env_from_secret_manager
 from app.routers import currency_router, expense_router
+import os
+
+APP_ENV = os.getenv("APP_ENV", "local")
+
+if APP_ENV not in {"local", "ci"}:
+    load_env_from_secret_manager(
+        secret_name="gdgteamf1-access-token",
+        project_id=os.environ["GCP_PROJECT_ID"],
+    )
 
 app = FastAPI()
 app.include_router(currency_router.router)
